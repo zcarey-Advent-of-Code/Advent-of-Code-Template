6bf98385492049ace53218addf4473a6ecd39697
zcareyltu <38868466+zcareyltu@users.noreply.github.com>
2021-12-01 01:09:24 -0500

    Added a comment

--- AdventOfCode/Parsing/Readers/LineReader.cs
+++ AdventOfCode/Parsing/Readers/LineReader.cs
@@ -7,6 +7,9 @@ namespace AdventOfCode.Parsing {
 
 	public class LineReader : IParser<StreamReader, IEnumerable<string>> {
 		override internal IEnumerable<string> Parse(StreamReader input) {
+			// We must read everything into memory so that IEnumerables act as expected later.
+			// If we don't, we won't be able to iterate over the values multiple times because the memory stream that is provided doesn't
+			// reset the position when enumerating multiple times.
 			List<string> lines = new();
 
 			string line;
